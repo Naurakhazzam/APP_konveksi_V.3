ea190f9838af6e7aef424b8992ffeff12f8a63a9
toph <taufikakbarhidayat@gmail.com>
2026-04-25 08:46:57 +0700

    chore: change packing scan mode to lanjut

--- src/app/(dashboard)/app/produksi/scan/packing/page.tsx
+++ src/app/(dashboard)/app/produksi/scan/packing/page.tsx
@@ -21,7 +21,7 @@ export default async function ScanPackingPage() {
         <ScanSimpleClient
           tahap="packing"
           tahapLabel="Packing"
-          mode="standard"
+          mode="lanjut"
         />
         <StageListSectionContainer
           tahap="packing"
